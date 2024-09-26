d6f760b387e143087076658210a0ae872af9c67b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-26 14:01:56 +0200

    Some updates in the application

--- casa_cultura-app/database/migrations/2024_09_26_094009_create_artists_table.php
+++ casa_cultura-app/database/migrations/2024_09_26_094009_create_artists_table.php
@@ -4,22 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
+            $table->string('Name');
+            $table->string('Address');
+            $table->integer('Cell_number');
+            $table->string('Activity');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('artists');
--- casa_cultura-app/database/migrations/2024_09_26_094024_create_events_table.php
+++ casa_cultura-app/database/migrations/2024_09_26_094024_create_events_table.php
@@ -4,22 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('Date');
+            $table->string('Location');
+            $table->integer('Number_of_speaker');
+            $table->time('Hour');
+            $table->string('Description');
+            $table->string('Event_picture');
+
+            //*Inicio das chaves estrangeiras
+            $table->unsignedBigInteger('id_artist');
+            $table->foreign('id_artist')->references('id')->on('artists');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('events');
