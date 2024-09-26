68260be42477789362f6f4e41796644a700811cd
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-26 11:27:27 +0200

    Changing names in the register page

--- casa_cultura-app/resources/views/auth/register.blade.php
+++ casa_cultura-app/resources/views/auth/register.blade.php
@@ -1,4 +1,110 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en-US" dir="ltr">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ERP - Casa de cultura</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="../../../assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="../../../assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+    <script src="../../../assets/js/config.js"></script>
+    <script src="../../../vendors/simplebar/simplebar.min.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap"
+        rel="stylesheet">
+    <link href="../../../vendors/simplebar/simplebar.min.css" rel="stylesheet">
+    <link href="../../../assets/css/theme-rtl.min.css" rel="stylesheet" id="style-rtl">
+    <link href="../../../assets/css/theme.min.css" rel="stylesheet" id="style-default">
+    <link href="../../../assets/css/user-rtl.min.css" rel="stylesheet" id="user-style-rtl">
+    <link href="../../../assets/css/user.min.css" rel="stylesheet" id="user-style-default">
+    <script>
+        var isRTL = JSON.parse(localStorage.getItem('isRTL'));
+        if (isRTL) {
+            var linkDefault = document.getElementById('style-default');
+            var userLinkDefault = document.getElementById('user-style-default');
+            linkDefault.setAttribute('disabled', true);
+            userLinkDefault.setAttribute('disabled', true);
+            document.querySelector('html').setAttribute('dir', 'rtl');
+        } else {
+            var linkRTL = document.getElementById('style-rtl');
+            var userLinkRTL = document.getElementById('user-style-rtl');
+            linkRTL.setAttribute('disabled', true);
+            userLinkRTL.setAttribute('disabled', true);
+        }
+    </script>
+</head>
+
+<body>
+    <main class="main" id="top">
+        <div class="container-fluid">
+            <div class="row min-vh-100 flex-center g-0">
+                <div class="col-lg-8 col-xxl-5 py-3 position-relative"><img class="bg-auth-circle-shape"
+                        src="../../../assets/img/icons/spot-illustrations/bg-shape.png" alt=""
+                        width="250"><img class="bg-auth-circle-shape-2"
+                        src="../../../assets/img/icons/spot-illustrations/shape-1.png" alt="" width="150">
+                    <div class="card overflow-hidden z-1">
+                        <div class="card-body p-4 p-sm-5">
+                            <div class="row g-0 h-100">
+                                <div class="col-md-12 d-flex flex-center">
+                                    <div class="p-4 p-md-5 flex-grow-1">
+                                        <h3>Registre-se</h3>
+                                        <form>
+                                            <div class="mb-3"><label class="form-label"
+                                                    for="card-name">Nome</label><input class="form-control"
+                                                    type="text" autocomplete="on" id="card-name" /></div>
+                                            <div class="mb-3"><label class="form-label" for="card-email">Endereço
+                                                    Email</label><input class="form-control" type="email"
+                                                    autocomplete="on" id="card-email" /></div>
+                                            <div class="row gx-2">
+                                                <div class="mb-3 col-sm-6"><label class="form-label"
+                                                        for="card-password">Password</label><input class="form-control"
+                                                        type="password" autocomplete="on" id="card-password" /></div>
+                                                <div class="mb-3 col-sm-6"><label class="form-label"
+                                                        for="card-confirm-password">Confirme o Password</label><input
+                                                        class="form-control" type="password" autocomplete="on"
+                                                        id="card-confirm-password" /></div>
+                                            </div>
+                                            {{-- <div class="form-check"><input class="form-check-input" type="checkbox"
+                                                    id="card-register-checkbox" /><label class="form-label"
+                                                    for="card-register-checkbox">I accept the <a href="#!">terms
+                                                    </a>and <a class="white-space-nowrap" href="#!">privacy
+                                                        policy</a></label></div> --}}
+                                            <div class="mb-3"><button class="btn btn-primary d-block w-100 mt-3"
+                                                    type="submit" name="submit">Register</button></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="../../../vendors/popper/popper.min.js"></script>
+    <script src="../../../vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="../../../vendors/anchorjs/anchor.min.js"></script>
+    <script src="../../../vendors/is/is.min.js"></script>
+    <script src="../../../vendors/fontawesome/all.min.js"></script>
+    <script src="../../../vendors/lodash/lodash.min.js"></script>
+    <script src="../../../vendors/list.js/list.min.js"></script>
+    <script src="../../../assets/js/theme.js"></script>
+</body>
+
+</html>
+
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +155,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
