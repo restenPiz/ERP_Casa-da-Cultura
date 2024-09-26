537dcc97e0f35a4c919389e1e1daf8fc3492a698
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-26 14:10:52 +0200

    Everthing is done with migration

--- casa_cultura-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ casa_cultura-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -17,14 +13,19 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('age');
+            $table->date('Date_of_birth');
+            $table->string('bi');
+            $table->string('place');
+            $table->integer('contact');
+            $table->string('user_type');
+            $table->string('upload_file');
+            $table->string('function');
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
