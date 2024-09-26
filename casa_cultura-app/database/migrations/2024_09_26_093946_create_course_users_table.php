3c1f0cd135de8041a033cadd2b606d286f6624ba
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-26 12:19:41 +0200

    Some updates in the application

--- casa_cultura-app/database/migrations/2024_09_26_093946_create_course_users_table.php
+++ casa_cultura-app/database/migrations/2024_09_26_093946_create_course_users_table.php
@@ -4,22 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+
     public function up(): void
     {
         Schema::create('course_users', function (Blueprint $table) {
             $table->id();
+
+            //*Inicio das chaves estrangeiras
+            $table->unsignedBigInteger('id_course');
+            $table->foreign('id_course')->references('id')->on('courses');
+
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('course_users');
