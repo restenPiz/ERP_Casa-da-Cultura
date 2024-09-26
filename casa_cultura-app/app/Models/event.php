7b8bf08da788e918623eeab046341e46fbf08326
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-26 11:51:46 +0200

    Some updates in the application

--- casa_cultura-app/app/Models/event.php
+++ casa_cultura-app/app/Models/event.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class event extends Model
 {
     use HasFactory;
+
+    protected $table = 'events';
+
+    protected $fillable = [
+        'Date',
+        'Location',
+        'Number_of_speaker',
+        'Hour',
+        'Description',
+        'Event_picture',
+        'Id_artist',
+    ];
 }
