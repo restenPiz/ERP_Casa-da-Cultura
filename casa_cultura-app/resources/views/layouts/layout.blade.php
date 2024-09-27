e0424c4d1ae7e9ea7066eacef8b6dfa590a95a59
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-27 08:33:30 +0200

    SOme updates in nthe application

--- casa_cultura-app/resources/views/layouts/layout.blade.php
+++ casa_cultura-app/resources/views/layouts/layout.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en-US" dir="ltr">
-<!-- Added by HTTrack -->
 <meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 
 <head>
@@ -8,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Falcon | Dashboard &amp; Web App Template</title>
+    <title>ERP - Casa_Cultura</title>
     <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
