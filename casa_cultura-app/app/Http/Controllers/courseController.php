75abb88ec142186f41f83b83a61703a3ad3a94f7
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-27 14:20:54 +0200

    Some updates in the applicatin

--- casa_cultura-app/app/Http/Controllers/courseController.php
+++ casa_cultura-app/app/Http/Controllers/courseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\course;
 use Illuminate\Http\Request;
 
 class courseController extends Controller
@@ -9,5 +10,16 @@ class courseController extends Controller
     public function index()
     {
         return view('coursePages.index');
+    }
+    public function storeCourse()
+    {
+        $courses = new course();
+
+        $courses->Course_name = Request::input('Course_name');
+        $courses->Description = Request::input('Description');
+        $courses->Price = Request::input('Price');
+        $courses->Goals = Request::input('Goals');
+
+        $courses->save();
     }
 }
