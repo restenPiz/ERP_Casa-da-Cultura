e6404db728f2dff2d28610aff4b4079eaece9d4d
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-27 11:22:06 +0200

    Some updates in the application

--- casa_cultura-app/routes/web.php
+++ casa_cultura-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\courseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //*Inicio das rotas da parte de cursos
+    Route::get('/courseMain', [courseController::class, 'index'])->name('course.index');
+
+    //*Inicio das rotas da parte de funcionarios
+
+    //*Inicio das rotas da parte de formadores
+
+    //*Inicio das rotas da parte de artista
 });
 
 require __DIR__ . '/auth.php';
