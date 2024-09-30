29d0416855e1d22b2eca198fdb82ac075b2395e0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-09-30 10:08:02 +0200

    Some updates in the application

--- casa_cultura-app/resources/views/trainerPages/index.blade.php
+++ casa_cultura-app/resources/views/trainerPages/index.blade.php
@@ -8,10 +8,10 @@
                 <div class="card-header position-relative min-vh-25 mb-8">
                   <div class="cover-image">
                     <div class="bg-holder rounded-3 rounded-bottom-0" style="background-image:url(../../assets/img/generic/4.jpg);"></div><!--/.bg-holder-->
-                    <input class="d-none" id="upload-cover-image" type="file" /><label class="cover-image-file-input" for="upload-cover-image"><span class="fas fa-camera me-2"></span><span>Change cover photo</span></label>
+                    {{-- <input class="d-none" id="upload-cover-image" type="file" /><label class="cover-image-file-input" for="upload-cover-image"><span class="fas fa-camera me-2"></span><span>Change cover photo</span></label> --}}
                   </div>
                   <div class="avatar avatar-5xl avatar-profile shadow-sm img-thumbnail rounded-circle">
-                    <div class="h-100 w-100 rounded-circle overflow-hidden position-relative"> <img src="../../assets/img/team/2.jpg" width="200" alt="" data-dz-thumbnail="data-dz-thumbnail" /><input class="d-none" id="profile-image" type="file" /><label class="mb-0 overlay-icon d-flex flex-center" for="profile-image"><span class="bg-holder overlay overlay-0"></span><span class="z-1 text-white dark__text-white text-center fs-10"><span class="fas fa-camera"></span><span class="d-block">Update</span></span></label></div>
+                    <div class="h-100 w-100 rounded-circle overflow-hidden position-relative"> <img src="../../assets/img/team/2.jpg" width="200" alt="" data-dz-thumbnail="data-dz-thumbnail" /><input name="upload_file" class="d-none" id="profile-image" type="file" /><label class="mb-0 overlay-icon d-flex flex-center" for="profile-image"><span class="bg-holder overlay overlay-0"></span><span class="z-1 text-white dark__text-white text-center fs-10"><span class="fas fa-camera"></span><span class="d-block">Update</span></span></label></div>
                   </div>
                 </div>
               </div>
@@ -25,13 +25,17 @@
                 </div>
                 <div class="card-body bg-body-tertiary">
                   <form class="row g-3">
-                    <div class="col-lg-6"> <label class="form-label" for="first-name">First Name</label><input class="form-control" id="first-name" type="text" value="Anthony" /></div>
-                    <div class="col-lg-6"> <label class="form-label" for="last-name">Last Name</label><input class="form-control" id="last-name" type="text" value="Hopkins" /></div>
-                    <div class="col-lg-6"> <label class="form-label" for="email1">Email</label><input class="form-control" id="email1" type="text" value="anthony@gmail.com" /></div>
-                    <div class="col-lg-6"> <label class="form-label" for="email2">Phone</label><input class="form-control" id="email2" type="text" value="+44098098304" /></div>
-                    <div class="col-lg-12"><label class="form-label" for="email3">Heading</label><input class="form-control" id="email3" type="text" value="Software Engineer" /></div>
-                    <div class="col-lg-12"> <label class="form-label" for="intro">Intro</label><textarea class="form-control" id="intro" name="intro" cols="30" rows="13">Dedicated, passionate, and accomplished Full Stack Developer with 9+ years of progressive experience working as an Independent Contractor for Google and developing and growing my educational social network that helps others learn programming, web design, game development, networking. I’ve acquired a wide depth of knowledge and expertise in using my technical skills in programming, computer science, software development, and mobile app development to developing solutions to help organizations increase productivity, and accelerate business performance. It’s great that we live in an age where we can share so much with technology but I’m but I’m ready for the next phase of my career, with a healthy balance between the virtual world and a workplace where I help others face-to-face. There’s always something new to learn, especially in IT-related fields. People like working with me because I can explain technology to everyone, from staff to executives who need me to tie together the details and the big picture. I can also implement the technologies that successful projects need.</textarea></div>
-                    <div class="col-12 d-flex justify-content-end"><button class="btn btn-primary" type="submit">Update </button></div>
+                    <div class="col-lg-6"> <label class="form-label" for="first-name">Primeiro Nome</label><input name="name" class="form-control" id="first-name" type="text" placeholder="Digite o Seu Nome" /></div>
+                    <div class="col-lg-6"> <label class="form-label" for="last-name">Apelido</label><input name="surname" class="form-control" id="last-name" type="text" placeholder="Digite o seu apelido" /></div>
+                    <div class="col-lg-6"> <label class="form-label" for="email1">Email</label><input name="email" placeholder="mauropeniel@gmail.com" class="form-control" id="email1" type="text" /></div>
+                    <div class="col-lg-6"> <label class="form-label" for="email2">Contacto</label><input class="form-control" id="email2" type="text" value="+258 " name="contact" /></div>
+                    <div class="col-lg-12"><label class="form-label" for="email3">Password</label><input placeholder="Digite a Sua Senha" class="form-control" id="email3" type="password"/></div>
+                    
+                    <div class="col-lg-12"> <label class="form-label" for="email2">Numero de BI</label><input class="form-control" id="email2" type="text" name="bi" placeholder="0290290927329BS"/></div>
+                    <div class="col-lg-6"> <label class="form-label" for="email2">Residencia</label><input class="form-control" id="email2" type="text" placeholder="Local de Residencia" name="place" /></div>
+                    <div class="col-lg-6"> <label class="form-label" for="email2">Cargo</label><input class="form-control" id="email2" type="text" placeholder="Digite o seu cargo" name="function" /></div>
+                    <div class="col-lg-6"> <label class="form-label" for="email2"></label><input class="form-control" value="trainer" id="email2" type="hidden" name="user_type" /></div>
+                    <div class="col-12 d-flex justify-content-end"><button class="btn btn-primary" type="submit">Adicionar Formador </button></div>
                   </form>
                 </div>
               </div>
