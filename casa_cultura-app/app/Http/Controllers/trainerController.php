55c25c69d3bc204089345481dc10b0040857ade4
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-01 11:12:23 +0200

    Some updates in the application

--- casa_cultura-app/app/Http/Controllers/trainerController.php
+++ casa_cultura-app/app/Http/Controllers/trainerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use DB;
 use Illuminate\Http\Request;
 
 class trainerController extends Controller
@@ -12,6 +14,10 @@ class trainerController extends Controller
     }
     public function all()
     {
-        return view('trainerPages.all');
+        $trainers = DB::table('users')
+            ->where('user_type', '=', 'Trainer')
+            ->get();
+
+        return view('trainerPages.all', compact('trainers'));
     }
 }
