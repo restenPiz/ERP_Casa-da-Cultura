6b68b12d88aed238fdaff61e9cca59fab7026e3f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-02 10:00:40 +0200

    Some updates in the application

--- casa_cultura-app/resources/views/trainerPages/all.blade.php
+++ casa_cultura-app/resources/views/trainerPages/all.blade.php
@@ -111,84 +111,91 @@
                                                         href="#!">{{ Auth::user()->name }}</a></p>
                                             </div>
                                             <div class="p-4">
-                                                <div class="row">
-                                                    <div class="col-12 d-flex justify-content-center mb-4">
-                                                        <div
-                                                            class="avatar avatar-5xl avatar-profile shadow-sm img-thumbnail rounded-circle">
+                                                <form action="{{route('trainer.update',['id'=>$trainer->id])}}" method="post">
+                                                    @csrf
+                                                    <div class="row">
+                                                        <div class="col-12 d-flex justify-content-center mb-4">
                                                             <div
-                                                                class="h-100 w-100 rounded-circle overflow-hidden position-relative">
-                                                                <!-- Imagem Atual -->
-                                                                <img src="{{ asset('storage/' . $trainer->upload_file) }}"
-                                                                    width="200" alt="Foto do Formador" />
-                                                                <!-- Input de Arquivo (Oculto) -->
-                                                                <input name="upload_file" class="d-none"
-                                                                    id="profile-image" type="file" accept="image/*" />
-                                                                <!-- Label que Atua como Botão para Selecionar a Imagem -->
-                                                                <label class="mb-0 overlay-icon d-flex flex-center"
-                                                                    for="profile-image">
-                                                                    <span class="bg-holder overlay overlay-0"></span>
-                                                                    <span
-                                                                        class="z-1 text-white dark__text-white text-center fs-10">
-                                                                        <span class="fas fa-camera"></span>
-                                                                        <span class="d-block">Adicionar Foto</span>
-                                                                    </span>
-                                                                </label>
+                                                                class="avatar avatar-5xl avatar-profile shadow-sm img-thumbnail rounded-circle">
+                                                                <div
+                                                                    class="h-100 w-100 rounded-circle overflow-hidden position-relative">
+                                                                    <!-- Imagem Atual -->
+                                                                    <img src="{{ asset('storage/' . $trainer->upload_file) }}"
+                                                                        width="200" alt="Foto do Formador" />
+                                                                    <!-- Input de Arquivo (Oculto) -->
+                                                                    <input name="upload_file" class="d-none"
+                                                                        id="profile-image" type="file"
+                                                                        accept="image/*" />
+                                                                    <!-- Label que Atua como Botão para Selecionar a Imagem -->
+                                                                    <label class="mb-0 overlay-icon d-flex flex-center"
+                                                                        for="profile-image">
+                                                                        <span class="bg-holder overlay overlay-0"></span>
+                                                                        <span
+                                                                            class="z-1 text-white dark__text-white text-center fs-10">
+                                                                            <span class="fas fa-camera"></span>
+                                                                            <span class="d-block">Adicionar Foto</span>
+                                                                        </span>
+                                                                    </label>
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="first-name">Primeiro Nome</label><input name="name"
-                                                            class="form-control" id="first-name" type="text"
-                                                            placeholder="Digite o Seu Nome" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="last-name">Apelido</label><input name="Surname"
-                                                            class="form-control" id="last-name" type="text"
-                                                            placeholder="Digite o seu apelido" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email1">Email</label><input name="email"
-                                                            placeholder="mauropeniel@gmail.com" class="form-control"
-                                                            id="email1" type="text" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email2">Contacto</label><input class="form-control"
-                                                            id="email2" type="text" value="258 "
-                                                            name="contact" />
-                                                    </div>
-                                                    <div class="col-lg-6"><label class="form-label"
-                                                            for="email3">Password</label><input name="password"
-                                                            placeholder="Digite a Sua Senha" class="form-control"
-                                                            id="email3" type="password" />
-                                                    </div>
-                                                    <div class="col-lg-6"><label class="form-label"
-                                                            for="email3">Password de Confirmacao</label><input
-                                                            name="password_confirmation" placeholder="Digite a Sua Senha"
-                                                            class="form-control" id="email3" type="password" />
-                                                    </div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="first-name">Primeiro Nome</label><input
+                                                                name="name" class="form-control" id="first-name"
+                                                                type="text" placeholder="Digite o Seu Nome" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="last-name">Apelido</label><input name="Surname"
+                                                                class="form-control" id="last-name" type="text"
+                                                                placeholder="Digite o seu apelido" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email1">Email</label><input name="email"
+                                                                placeholder="mauropeniel@gmail.com" class="form-control"
+                                                                id="email1" type="text" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2">Contacto</label><input class="form-control"
+                                                                id="email2" type="text" value="258 "
+                                                                name="contact" />
+                                                        </div>
+                                                        <div class="col-lg-6"><label class="form-label"
+                                                                for="email3">Password</label><input name="password"
+                                                                placeholder="Digite a Sua Senha" class="form-control"
+                                                                id="email3" type="password" />
+                                                        </div>
+                                                        <div class="col-lg-6"><label class="form-label"
+                                                                for="email3">Password de Confirmacao</label><input
+                                                                name="password_confirmation"
+                                                                placeholder="Digite a Sua Senha" class="form-control"
+                                                                id="email3" type="password" />
+                                                        </div>
 
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email2">Numero de BI</label><input class="form-control"
-                                                            id="email2" type="text" name="bi"
-                                                            placeholder="0290290927329BS" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label" for="email2">Data
-                                                            de Nascimento</label><input class="form-control"
-                                                            id="email2" type="date" name="Date_of_birth" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email2">Residencia</label><input class="form-control"
-                                                            id="email2" type="text"
-                                                            placeholder="Local de Residencia" name="place" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email2">Cargo</label><input class="form-control"
-                                                            id="email2" type="text"
-                                                            placeholder="Digite o seu cargo" name="function" /></div>
-                                                    <div class="col-lg-6"> <label class="form-label"
-                                                            for="email2"></label><input class="form-control"
-                                                            value="Trainer" id="email2" type="hidden"
-                                                            name="user_type" />
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2">Numero de BI</label><input
+                                                                class="form-control" id="email2" type="text"
+                                                                name="bi" placeholder="0290290927329BS" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2">Data
+                                                                de Nascimento</label><input class="form-control"
+                                                                id="email2" type="date" name="Date_of_birth" />
+                                                        </div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2">Residencia</label><input
+                                                                class="form-control" id="email2" type="text"
+                                                                placeholder="Local de Residencia" name="place" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2">Cargo</label><input class="form-control"
+                                                                id="email2" type="text"
+                                                                placeholder="Digite o seu cargo" name="function" /></div>
+                                                        <div class="col-lg-6"> <label class="form-label"
+                                                                for="email2"></label><input class="form-control"
+                                                                value="Trainer" id="email2" type="hidden"
+                                                                name="user_type" />
+                                                        </div>
+                                                        <div class="col-12 d-flex justify-content-end"><button
+                                                                class="btn btn-primary" type="submit">Adicionar Formador
+                                                            </button></div>
                                                     </div>
-                                                    <div class="col-12 d-flex justify-content-end"><button
-                                                            class="btn btn-primary" type="submit">Adicionar Formador
-                                                        </button></div>
-                                                </div>
                                             </div>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
