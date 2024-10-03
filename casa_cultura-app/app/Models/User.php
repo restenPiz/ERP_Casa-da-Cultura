86eb53d42c5396d4cd1f5539b49881f9dfed831a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-03 15:24:42 +0200

    Trying to fix these error

--- casa_cultura-app/app/Models/User.php
+++ casa_cultura-app/app/Models/User.php
@@ -41,6 +41,6 @@ class User extends Authenticatable
 
     public function course_user()
     {
-        return $this->belongToMany(course::class, 'course_user', 'id_user', 'id');
+        return $this->belongToMany(course::class, 'course_user', 'id_course', 'id');
     }
 }
