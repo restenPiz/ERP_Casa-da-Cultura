41822b7d34205bc9a13dcda5e1664735647413fa
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-09 19:11:44 +0200

    Some updates in the application

--- casa_cultura-app/app/Http/Controllers/chapterController.php
+++ casa_cultura-app/app/Http/Controllers/chapterController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chapter;
 use Illuminate\Http\Request;
 
 class chapterController extends Controller
 {
     public function store()
     {
-
+        $chapter = new Chapter();
     }
 }
