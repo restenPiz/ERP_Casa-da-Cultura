e25baba2d52e3ef114a67036d5e190d951b3c808
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-09 19:13:54 +0200

    Commit number 2000

--- casa_cultura-app/app/Http/Controllers/chapterController.php
+++ casa_cultura-app/app/Http/Controllers/chapterController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Chapter;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class chapterController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
         $chapter = new Chapter();
+
+        $chapter->Title = $request->input('Title');
+        $chapter->Description = $request->input('Description');
+        $chapter->id_course = $request->input('id_course');
+
+        $chapter->save();
+
+        Alert::success('Adicionado!', 'O capitulo foi adicionado com sucesso!');
+
+        return back();
     }
 }
