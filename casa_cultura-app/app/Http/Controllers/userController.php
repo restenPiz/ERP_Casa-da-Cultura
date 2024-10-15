9802887a75721df21e46f6190d58f150756aea51
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-15 14:03:40 +0200

    Some updates in the application

--- casa_cultura-app/app/Http/Controllers/userController.php
+++ casa_cultura-app/app/Http/Controllers/userController.php
@@ -15,124 +15,55 @@ class userController extends Controller
 {
     public function storeUser(Request $request)
     {
-        // dd($request->all());
-        // $validatedData = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'Surname' => 'required|string|max:255',
-        //     'email' => [
-        //         'required',
-        //         'string',
-        //         'email',
-        //         'max:255',
-        //     ],
-        //     'id_course' => 'required|array',
-        //     'id_course.*' => 'exists:courses,id',
-        //     'password' => 'required|string|min:8|confirmed',
-        //     'Date_of_birth' => 'nullable|date',
-        //     'bi' => 'nullable|string|max:50',
-        //     'place' => 'nullable|string|max:255',
-        //     'contact' => 'nullable|string|max:20',
-        //     'upload_file' => 'nullable|file|mimes:pdf,doc,docx,jpg,png|max:10240',
-        //     'function' => 'nullable|string|max:255',
-        //     'user_type' => [
-        //         'required',
-        //         'string',
-        //     ],
-        // ]);
-        dd($request->all());
-        // $validatedData = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'Surname' => 'required|string|max:255',
-        //     'email' => [
-        //         'required',
-        //         'string',
-        //         'email',
-        //         'max:255',
-        //         'unique:users,email', // Evita duplicatas de email
-        //     ],
-        //     'password' => 'required|string|min:8|confirmed',
-        //     'Date_of_birth' => 'nullable|date',
-        //     'bi' => 'nullable|string|max:50',
-        //     'contact' => 'nullable|string|max:20',
-        //     'upload_file' => 'nullable|file|mimes:pdf,doc,docx,jpg,png|max:10240',
-        //     'user_type' => [
-        //         'required',
-        //         'string',
-        //         'in:Employee,Trainer,Users', // Valida os tipos de usuário permitidos
-        //     ],
-        //     'id_course' => 'required_if:user_type,User|exists:courses,id', // Validação condicional
-        // ]);
+        $user = new User();
 
-        // if ($request->hasFile('upload_file')) {
-        //     $validatedData['upload_file'] = $request->file('upload_file')->store('uploads/files', 'public');
-        // }
+        switch ($request->input('user_type')) {
+            case 'Employee':
+                $role = 'employee';
+                $successMessage = 'O usuário funcionário foi adicionado com sucesso!';
+                break;
+            case 'Trainer':
+                $role = 'trainer';
+                $successMessage = 'O usuário formador foi adicionado com sucesso!';
+                break;
+            case 'Users':
+                $role = 'users';
+                $successMessage = 'O aluno foi adicionado com sucesso!';
+                break;
+            default:
+                $role = null;
+                $successMessage = 'Usuário adicionado com sucesso!';
+                break;
+        }
 
-        // $user = User::create([
-        //     'name' => $validatedData['name'],
-        //     'Surname' => $validatedData['Surname'],
-        //     'email' => $validatedData['email'],
-        //     'user_type' => $validatedData['user_type'],
-        //     'password' => Hash::make($validatedData['password']),
-        //     'Date_of_birth' => $validatedData['Date_of_birth'],
-        //     'bi' => $validatedData['bi'],
-        //     'contact' => $validatedData['contact'],
-        //     'upload_file' => $validatedData['upload_file'],
-        // ]);
+        $user->name = $request->input('name');
+        $user->Surname = $request->input('Surname');
+        $user->email = $request->input('email');
+        $user->user_type = $request->input('user_type');
+        $user->password = $request->input('password');
+        $user->Date_of_birth = $request->input('Date_of_birth');
+        $user->bi = $request->input('bi');
+        $user->contact = $request->input('contact');
+        $user->upload_file = $request->input('upload_file');
 
-        // switch ($validatedData['user_type']) {
-        //     case 'Employee':
-        //         $role = 'employee';
-        //         $successMessage = 'O usuário funcionário foi adicionado com sucesso!';
-        //         break;
-        //     case 'Trainer':
-        //         $role = 'trainer';
-        //         $successMessage = 'O usuário formador foi adicionado com sucesso!';
-        //         break;
-        //     case 'Users':
-        //         $role = 'users';
-        //         $successMessage = 'O aluno foi adicionado com sucesso!';
-        //         break;
-        //     default:
-        //         $role = null;
-        //         $successMessage = 'Usuário adicionado com sucesso!';
-        //         break;
-        // }
+        if ($request->hasFile('upload_file')) {
+            $user['upload_file'] = $request->file('upload_file')->store('uploads/files', 'public');
+        }
 
-        // //*Metodo de adicao de roles no usuario
-        // $user->addRole($role);
+        $user->save();
 
-        // Alert::success('Adicionado', $successMessage);
+        $user->addRole('users');
+        $user->courses()->attach($request->input('id_course'));
 
-        // return back();
+        Alert::success('Adicionado!', $successMessage);
+
+        return back();
     }
     public function update(Request $request, $id)
     {
         //*Finding an user
         $user = User::findOrFail($id);
 
-        // $validatedData = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'Surname' => 'required|string|max:255',
-        //     'email' => [
-        //         'required',
-        //         'string',
-        //         'email',
-        //         'max:255',
-        //         Rule::unique('users', 'email')->ignore($user->id),
-        //     ],
-        //     'password' => 'nullable|string|min:8|confirmed',
-        //     'Date_of_birth' => 'nullable|date',
-        //     'bi' => 'nullable|string|max:50',
-        //     'place' => 'nullable|string|max:255',
-        //     'contact' => 'nullable|string|max:20',
-        //     'upload_file' => 'nullable|file|mimes:jpg,jpeg,png,gif|max:10240', // 10MB
-        //     'function' => 'nullable|string|max:255',
-        //     'user_type' => [
-        //         'required',
-        //         'string',
-        //         Rule::in(['Employee', 'Trainer', 'User']),
-        //     ],
-        // ]);
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'Surname' => 'required|string|max:255',
