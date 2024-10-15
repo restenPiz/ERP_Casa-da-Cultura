e293d346b5317ea7e424f0342f695948db367300
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-15 11:47:58 +0200

    Some updates in the app

--- casa_cultura-app/app/Http/Controllers/studentController.php
+++ casa_cultura-app/app/Http/Controllers/studentController.php
@@ -98,6 +98,7 @@ class studentController extends Controller
         //*Finding an user
         $user = User::findOrFail($id);
 
+        //*Inicio do metodo de validacao
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'Surname' => 'required|string|max:255',
