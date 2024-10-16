3b6e6fd7fb9bf5ec955649063759e3364e5699fa
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-16 11:39:20 +0200

    Some updates in the app

--- casa_cultura-app/resources/views/websitePages/eventDetail.blade.php
+++ casa_cultura-app/resources/views/websitePages/eventDetail.blade.php
@@ -1,6 +1,179 @@
-@extends('layouts.webLayout')
+@extends('layouts.detailLayout')
 @section('content')
+    <div class="edu-breadcrumb-area breadcrumb-style-1 ptb--60 ptb_md--40 ptb_sm--40 bg-image">
+        <div class="container eduvibe-animated-shape">
+           
+            <div class="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+                <div class="shape-dot-wrapper shape-wrapper d-xl-block d-none">
+                    <div class="shape-image shape-image-1">
+                        <img src="../asset/images/shapes/shape-11-07.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-2">
+                        <img src="../asset/images/shapes/shape-01-02.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-3">
+                        <img src="../asset/images/shapes/shape-03.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-4">
+                        <img src="../asset/images/shapes/shape-13-12.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-5">
+                        <img src="../asset/images/shapes/shape-36.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-6">
+                        <img src="../asset/images/shapes/shape-05-07.png" alt="Shape Thumb" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <h1>Detalhes do Evento</h1>
+    <div class="edu-event-details-area edu-event-details edu-section-gap bg-color-white">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-12">
+                    <div class="thumbnail">
+                        <img src="{{ asset('storage/' . $event->Event_picture) }}" alt="Event Images">
+                    </div>
+                </div>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <div class="content">
+                        <h3 class="title">Descricao do Evento</h3>
+                        <p style="text-align: justify">{{$event->Description}}</p>
+                        <ul>
+                            <li>Artista Convidado: <b>{{ $event->artists->Name }}</b></li>
+                            <li>Numero de Palestrantes: <b>{{ $event->Number_of_speaker }}</b></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="eduvibe-sidebar">
+                        <div class="eduvibe-widget eduvibe-widget-details">
+                            <h5 class="title">Detalhes do Evento</h5>
+                            <div class="widget-content">
+                                <ul>
+                                    <li><span><i class="icon-calendar-2-line"></i> Data do Evento</span><span>{{ \Carbon\Carbon::parse($event->Date)->format('d-M-Y') }}</span></li>
+                                    <li><span><i class="icon-time-line"></i> Horario</span><span> {{ \Carbon\Carbon::parse($event->Hour)->format('H:i') }}</span></li>
+                                    <li><span><i class="icon-map-pin-line"></i> Localizacao</span><span>{{$event->Location}}</span>
+                                    </li>
+                                </ul>
 
+                                <div class="read-more-btn mt--45">
+                                    <a class="edu-btn btn-bg-alt w-100 text-center">Preco: {{$event->Price}} MZN</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Footer Area  -->
+    <footer class="eduvibe-footer-one edu-footer footer-style-default">
+        <div class="footer-top">
+            <div class="container eduvibe-animated-shape">
+                <div class="row g-5">
+                    <div class="col-lg-3 col-md-6 col-sm-12 col-12">
+                        <div class="edu-footer-widget">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img class="logo-light" src="../asset/images/logo/logo-white.png" alt="Site Logo">
+                                </a>
+                            </div>
+                            <p class="description">It is a long established fact that a reader will be distracted by the
+                                readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
+                            <ul class="social-share">
+                                <li><a href="#"><i class="icon-Fb"></i></a></li>
+                                <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                                <li><a href="#"><i class="icon-Pinterest"></i></a></li>
+                                <li><a href="#"><i class="icon-Twitter"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="edu-footer-widget explore-widget">
+                            <h5 class="widget-title">Explore</h5>
+                            <div class="inner">
+                                <ul class="footer-link link-hover">
+                                    <li><a href="about-us-1.html"><i class="icon-Double-arrow"></i>About Us</a></li>
+                                    <li><a href="event-list.html"><i class="icon-Double-arrow"></i>Upcoming Events</a>
+                                    </li>
+                                    <li><a href="blog-standard.html"><i class="icon-Double-arrow"></i>Blog & News</a></li>
+                                    <li><a href="faq.html"><i class="icon-Double-arrow"></i>FAQ Question</a></li>
+                                    <li><a href="testimonial.html"><i class="icon-Double-arrow"></i>Testimonial</a></li>
+                                    <li><a href="privacy-policy.html"><i class="icon-Double-arrow"></i>Privacy Policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="edu-footer-widget quick-link-widget">
+                            <h5 class="widget-title">Useful Links</h5>
+                            <div class="inner">
+                                <ul class="footer-link link-hover">
+                                    <li><a href="contact-us.html"><i class="icon-Double-arrow"></i>Contact Us</a></li>
+                                    <li><a href="pricing.html"><i class="icon-Double-arrow"></i>Pricing Plan</a></li>
+                                    <li><a href="instructor-profile.html"><i class="icon-Double-arrow"></i>Instructor
+                                            Profile</a></li>
+                                    <li><a href="faq.html"><i class="icon-Double-arrow"></i>FAQ</a></li>
+                                    <li><a href="course-style-3.html"><i class="icon-Double-arrow"></i>Popular Courses</a>
+                                    </li>
+                                    <li><a href="purchase-guide.html"><i class="icon-Double-arrow"></i>Terms &
+                                            Conditions</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="edu-footer-widget">
+                            <h5 class="widget-title">Contact Info</h5>
+                            <div class="inner">
+                                <div class="widget-information">
+                                    <ul class="information-list">
+                                        <li><i class="icon-map-pin-line"></i>275 Quadra Street Victoria Road, New York
+                                        </li>
+                                        <li><i class="icon-phone-fill"></i><a href="tel:+1(237)382-2839">+ 1 (237)
+                                                382-2839</a></li>
+                                        <li><i class="icon-phone-fill"></i><a href="tel:+1(237)382-2840">+ 1 (237)
+                                                382-2840</a></li>
+                                        <li><i class="icon-mail-line-2"></i><a target="_blank"
+                                                href="mailto:yourmailaddress@example.com">yourmailaddress@example.com</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="shape-dot-wrapper shape-wrapper d-md-block d-none">
+                    <div class="shape-image shape-image-1">
+                        <img src="../asset/images/shapes/shape-21-01.png" alt="Shape Thumb" />
+                    </div>
+                    <div class="shape-image shape-image-2">
+                        <img src="../asset/images/shapes/shape-35.png" alt="Shape Thumb" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area copyright-default">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="inner text-center">
+                            <p>Copyright 2023 <a href="#">EduVibe</a> Designed By <a
+                                    href="https://themeforest.net/user/devsvibe">DevsVibe</a>. All Rights Reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 @endsection
