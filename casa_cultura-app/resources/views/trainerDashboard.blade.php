83e67a38dcae15b996a5dc0c61b8ba8ff4563c4c
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-18 09:35:23 +0200

    Update trainerDashboard.blade.php

--- casa_cultura-app/resources/views/trainerDashboard.blade.php
+++ casa_cultura-app/resources/views/trainerDashboard.blade.php
@@ -5,19 +5,10 @@
         <div class="card-header">
             <div class="row">
                 <div class="col">
-                    <h5 class="mb-2">{{Auth::user()->name}} {{Auth::user()->Surname}} (<a href="mailto:{{Auth::user()->email}}">{{Auth::user()->email}}</a>)</h5><a
-                        class="btn btn-falcon-default btn-sm" href="#!"><span class="fas fa-plus fs-11 me-1"></span>Add
-                        note</a><button class="btn btn-falcon-default btn-sm dropdown-toggle ms-2 dropdown-caret-none"
-                        type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span
-                            class="fas fa-ellipsis-h"></span></button>
-                    <div class="dropdown-menu"><a class="dropdown-item" href="#">Edit</a><a class="dropdown-item"
-                            href="#">Report</a><a class="dropdown-item" href="#">Archive</a>
-                        <div class="dropdown-divider"></div><a class="dropdown-item text-danger" href="#">Delete
-                            user</a>
-                    </div>
+                    <h5 class="mb-2">{{Auth::user()->name}} {{Auth::user()->Surname}} (<a href="mailto:{{Auth::user()->email}}">{{Auth::user()->email}}</a>)</h5>
                 </div>
                 <div class="col-auto d-none d-sm-block">
-                    <h6 class="text-uppercase text-600">Customer<span class="fas fa-user ms-2"></span></h6>
+                    <h6 class="text-uppercase text-600">Formador<span class="fas fa-user ms-2"></span></h6>
                 </div>
             </div>
         </div>
@@ -25,7 +16,7 @@
             <div class="d-flex"><span class="fas fa-user text-success me-2" data-fa-transform="down-5"></span>
                 <div class="flex-1">
                     <p class="mb-0">Formador foi criado em</p>
-                    <p class="fs-10 mb-0 text-600">Jan 12, 11:13 PM</p>
+                    <p class="fs-10 mb-0 text-600">{{ \Carbon\Carbon::parse(Auth::user()->created_at)->format('d-M-y') }}, {{ \Carbon\Carbon::parse(Auth::user()->created_at)->format('H:i') }}</p>
                 </div>
             </div>
         </div>
@@ -34,88 +25,45 @@
         <div class="card-header">
             <div class="row align-items-center">
                 <div class="col">
-                    <h5 class="mb-0">Details</h5>
+                    <h5 class="mb-0">Detalhes</h5>
                 </div>
-                <div class="col-auto"><a class="btn btn-falcon-default btn-sm" href="#!"><span
-                            class="fas fa-pencil-alt fs-11 me-1"></span>Update details</a></div>
             </div>
         </div>
         <div class="card-body bg-body-tertiary border-top">
             <div class="row">
                 <div class="col-lg col-xxl-5">
-                    <h6 class="fw-semi-bold ls mb-3 text-uppercase">Account Information</h6>
+                    <h6 class="fw-semi-bold ls mb-3 text-uppercase">Detalhes de Conta</h6>
                     <div class="row">
                         <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">ID</p>
+                            <p class="fw-semi-bold mb-1">Name Completo</p>
                         </div>
-                        <div class="col">dcfasyo_Dfdjl</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">Created</p>
-                        </div>
-                        <div class="col">2019/01/12 23:13</div>
+                        <div class="col">{{Auth::user()->name}} {{Auth::user()->Surname}}</div>
                     </div>
                     <div class="row">
                         <div class="col-5 col-sm-4">
                             <p class="fw-semi-bold mb-1">Email</p>
                         </div>
-                        <div class="col"><a href="mailto:tony@gmail.com">tony@gmail.com</a></div>
+                        <div class="col"><a href="mailto:tony@gmail.com">{{Auth::user()->email}}</a></div>
                     </div>
                     <div class="row">
                         <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">Description</p>
+                            <p class="fw-semi-bold mb-1">Descricao</p>
                         </div>
                         <div class="col">
-                            <p class="fst-italic text-400 mb-1">No Description</p>
+                            <p class="fst-italic text-400 mb-1">Sem Descricao</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-0">VAT number</p>
+                            <p class="fw-semi-bold mb-0">Numero</p>
                         </div>
                         <div class="col">
-                            <p class="fst-italic text-400 mb-0">No VAT number</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg col-xxl-5 mt-4 mt-lg-0 offset-xxl-1">
-                    <h6 class="fw-semi-bold ls mb-3 text-uppercase">Billing Information</h6>
-                    <div class="row">
-                        <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">Send email to</p>
-                        </div>
-                        <div class="col"><a href="mailto:tony@gmail.com">tony@gmail.com</a></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">Address</p>
-                        </div>
-                        <div class="col">
-                            <p class="mb-1">8962 Lafayette St. <br />Oswego, NY 13126</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-1">Phone number</p>
-                        </div>
-                        <div class="col"><a href="tel:+12025550110">+1-202-555-0110</a></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5 col-sm-4">
-                            <p class="fw-semi-bold mb-0">Invoice prefix</p>
-                        </div>
-                        <div class="col">
-                            <p class="fw-semi-bold mb-0">7C23435</p>
+                            <p class="fst-italic text-400 mb-0">+258 {{Auth::user()->contact}}</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="card-footer border-top text-end"><a class="btn btn-falcon-default btn-sm" href="#!"><span
-                    class="fas fa-dollar-sign fs-11 me-1"></span>Refund</a><a class="btn btn-falcon-default btn-sm ms-2"
-                href="#!"><span class="fas fa-check fs-11 me-1"></span>Save changes</a></div>
-    </div> 
     </div>
 
     {{-- * Fim do conteudo da dash --}}
