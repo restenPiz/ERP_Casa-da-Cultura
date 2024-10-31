65b6d94fa53af32c54266f81b37de843e3577bc3
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-10-31 22:49:33 +0200

    Some updates in the application

--- casa_cultura-app/database/migrations/2024_10_31_204455_create_values_table.php
+++ casa_cultura-app/database/migrations/2024_10_31_204455_create_values_table.php
@@ -4,22 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('values', function (Blueprint $table) {
             $table->id();
+            $table->string('First');
+            $table->string('Second');
+            $table->string('Third');
+
+            //*Inicio das chaves estrangeiras
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('values');
