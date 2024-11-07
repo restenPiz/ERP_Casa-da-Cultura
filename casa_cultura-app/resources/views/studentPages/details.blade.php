bb5454ee096f34a4a1c96a96f74c8ecb036e5104
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-11-07 09:51:15 +0200

    Some updates in the application

---
+++ casa_cultura-app/resources/views/studentPages/details.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+@section('content')
+
+    {{--! Inicio do conteudo da minha pagina de detalhes--}}
+
+    
+    {{--! Fim do conteudo da minha pagina de detalhes--}}
+
+@endsection
