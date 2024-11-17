b70a5912388144d1402934be6d7cd9bb057955d6
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-11-17 22:28:19 +0200

    Some updates in the application

--- casa_cultura-app/resources/views/layouts/layout.blade.php
+++ casa_cultura-app/resources/views/layouts/layout.blade.php
@@ -7,12 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ERP - Casa da Cultura</title>
+    <title>Casa da Cultura</title>
     <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
+    {{-- <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png"> --}}
     {{-- <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico"> --}}
-    <link rel="shortcut icon" type="image/x-icon" href="../asset/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="asset/images/favicon.png">
     <meta name="msapplication-TileImage" content="../assets/img/favicons/mstile-150x150.png">
     <meta name="theme-color" content="#ffffff">
     <script src="../assets/js/config.js"></script>
