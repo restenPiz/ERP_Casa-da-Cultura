1c1df531502e23207ed0c4b82534f3cafd62bbde
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-12-17 09:20:42 +0200

    Some updates in the app

--- casa_cultura-app/resources/views/layouts/detailLayout.blade.php
+++ casa_cultura-app/resources/views/layouts/detailLayout.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="asset/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../asset/images/favicon.png">
     <link rel="stylesheet" href="../asset/css/vendor/bootstrap.min.css">
     <link rel="stylesheet" href="../asset/css/vendor/remixicon.css">
     <link rel="stylesheet" href="../asset/css/vendor/eduvibe-font.css">
--- casa_cultura-app/resources/views/layouts/layout.blade.php
+++ casa_cultura-app/resources/views/layouts/layout.blade.php
@@ -12,7 +12,7 @@
     {{-- <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png"> --}}
     {{-- <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico"> --}}
-    <link rel="shortcut icon" type="image/x-icon" href="asset/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../asset/images/favicon.png">
     <meta name="msapplication-TileImage" content="../assets/img/favicons/mstile-150x150.png">
     <meta name="theme-color" content="#ffffff">
     <script src="../assets/js/config.js"></script>
